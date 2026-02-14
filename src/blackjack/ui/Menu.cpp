c09f4858e7a535f698008e0053bf1863ac46c877
Evgen <dinobunny.re@gmail.com>
2026-02-14 23:44:00 +0100

    Removed background logic from the stylesheet code.

--- src/blackjack/ui/Menu.cpp
+++ src/blackjack/ui/Menu.cpp
@@ -4,17 +4,11 @@
 
 #include "ui_Menu.h"
 
-#include <QPixmap>
-
 Menu::Menu(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui::Menu)
 {
     ui->setupUi(this);
-
-    // Background
-    QPixmap bg(R"(:/assets/ui/assets/ui/menu.png)");
-    ui->label->setPixmap(bg);
 }
 
 Menu::~Menu()
